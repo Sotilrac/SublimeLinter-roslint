effb60f0801c0d936caa315e223c689522f144a6
Carlos Asmat <carlos.asmat@piaggiofastforward.com>
2018-07-11 18:35:19 -0400

    removes superfluous

--- linter.py
+++ linter.py
@@ -16,7 +16,7 @@ import os
 class Roslint(Linter):
     """Provides an interface to roslint."""
     distro = os.environ["ROS_DISTRO"]
-    cmd = ('/opt/ros/{}/lib/roslint/cpplint'.format(distro), '-', '$temp_file')
+    cmd = ('/opt/ros/{}/lib/roslint/cpplint'.format(distro), '-')
     regex = r'^.+:(?P<line>\d+):\s+(?P<message>.+)\s+\s\[(?P<code>.+)\]\s\[(?P<col>\d+)\]'
     error_stream = STREAM_STDERR
     defaults = {
